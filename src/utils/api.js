0700dba902734db07699b5bcb86a569bec89880f
IMing <hobnob848484@gmail.com>
2024-11-07 22:06:57 +0800

    fix: fix endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 export const analyzePatent = async (patent_id, company_name) => {
-  return await fetch('http://localhost:5000/analyze', {
+  return await fetch('http://54.205.200.84/:5000/analyze', {
     method: 'POST',
     headers: { 
       'Content-Type': 'application/json',
