cb1a75f92aaf6a2774bf52280fdac572d56a9272
IMing <hobnob848484@gmail.com>
2024-11-07 19:48:41 +0800

    fix: fix cors

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,10 @@
 export const analyzePatent = async (patent_id, company_name) => {
   return await fetch('http://54.205.200.84:5000/analyze', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify({ patent_id, company_name }),
   });
 };
